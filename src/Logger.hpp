4c0d922826676798f97e03881b8aa235f0ae86aa
Yimura <andreas.maerten@scarlet.be>
2022-02-17 18:30:03 +0100

    feat(Logger): Finished console out logging

--- src/Logger.hpp
+++ src/Logger.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include "common.hpp"
 #include "FileManager.hpp"
+#include <g3log/g3log.hpp>
+#include <g3log/loglevels.hpp>
+#include <g3log/logworker.hpp>
 
 namespace change_me
 {
@@ -19,19 +22,27 @@ namespace change_me
 		File m_FilePath;
 		std::ofstream m_FileOut;
 
+		std::unique_ptr<g3::LogWorker> m_Worker;
+
 	public:
 		Logger(std::string_view consoleTitle, File filePath, bool attachConsole = true)
-			: m_ConsoleTitle(consoleTitle), m_FilePath(filePath), m_AttachConsole(attachConsole), m_DidConsoleExist(false)
+			: m_ConsoleTitle(consoleTitle), m_FilePath(filePath), m_AttachConsole(attachConsole),
+			  m_DidConsoleExist(false), m_Worker(g3::LogWorker::createLogWorker())
 		{
 			InitConsole(m_AttachConsole);
 			OpenLogFile();
 
+			m_Worker->addSink(std::make_unique<LogSink>(), &LogSink::Callback);
+			g3::initializeLogging(m_Worker.get());
+
 			g_log = this;
 		}
+
 		~Logger()
 		{
 			g_log = nullptr;
 
+			m_Worker.reset();
 			DestroyConsole();
 		}
 
@@ -67,7 +78,7 @@ namespace change_me
 
 				SetConsoleMode(m_ConsoleHandle, console_mode);
 
-				m_ConsoleOut.open("$CONOUT", std::ios_base::out | std::ios_base::app);
+				m_ConsoleOut.open("CONOUT$", std::ios_base::out | std::ios_base::app);
 			}
 		}
 
@@ -77,5 +88,53 @@ namespace change_me
 			
 		}
 
+		struct LogSink
+		{
+			enum class Color
+			{
+				RESET,
+				WHITE = 97,
+				BLUE = 34,
+				GREEN = 32,
+				YELLOW = 33,
+				RED = 31
+			};
+
+			static Color GetColor(const LEVELS level)
+			{
+				switch (level.value)
+				{
+				case g3::kDebugValue:
+					return Color::BLUE;
+				case g3::kInfoValue:
+					return Color::GREEN;
+				case g3::kWarningValue:
+					return Color::YELLOW;
+				}
+				return g3::internal::wasFatal(level) ? Color::RED : Color::WHITE;
+			}
+
+			void Callback(g3::LogMessageMover log)
+			{
+				g3::LogMessage msg = log.get();
+
+				g_log->m_ConsoleOut << msg.toString(LogSink::FormatConsole) << std::flush;
+			}
+
+			static std::string FormatConsole(const g3::LogMessage& msg)
+			{
+				Color color = LogSink::GetColor(msg._level);
+				std::stringstream out;
+
+				out
+					<< "[" << msg.timestamp("%H:%M:%S") << "]"
+					<< "\x1b[" << (int)color << "m"
+					<< "[" << msg.level() << "]"
+					<< "\x1b[" << (int)Color::RESET << "m"
+					<< ": ";
+
+				return out.str();
+			}
+		};
 	};
 }
