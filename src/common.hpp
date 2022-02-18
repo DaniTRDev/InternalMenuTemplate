61b68a720658f06c260a37e1dedd242975a4fdfd
danitrchetadas <danitrchetadas@gmail.com>
2022-02-18 14:09:25 +0100

    feat(Common.hpp): Added Logger & Component system includes to be precompiled

--- src/common.hpp
+++ src/common.hpp
@@ -17,6 +17,11 @@
 #include <string>
 #include <string_view>
 
+#include "Logger.hpp" /*logger should not be modified very often*/
+
+#include "ComponentManager/ComponentBase.hpp" /*the component system should not even be modified*/
+#include "ComponentManager/ComponentManager.hpp"
+
 namespace change_me
 {
 	using namespace std::chrono_literals;
