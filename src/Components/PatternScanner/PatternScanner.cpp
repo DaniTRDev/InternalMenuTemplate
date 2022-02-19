7ce09f9eb88a4244cf2a7d609e2539e7ee0a81e4
danitrchetadas <danitrchetadas@gmail.com>
2022-02-19 15:56:54 +0100

    fix(PatternScanner.cpp): Fixed TransformPatternStrToBytes, Scan and made the Initializer add the main module from a global variable (g_GameModuleName)

--- src/Components/PatternScanner/PatternScanner.cpp
+++ src/Components/PatternScanner/PatternScanner.cpp
@@ -2,7 +2,7 @@
 
 namespace change_me
 {
-	Pattern::Pattern(std::string_view Name, std::string_view PatternStr) : m_Name(Name), m_PatternStr(PatternStr)
+	Pattern::Pattern(std::string_view Name, std::string_view PatternStr) : m_Name(Name), m_PatternStr(PatternStr), m_Ptr(0)
 	{
 		if (PatternStr.length() == 0)
 		{
@@ -23,24 +23,28 @@ namespace change_me
 
 	void Pattern::TransforPatternStrToBytes()
 	{
-		for (auto i = 0; i < m_Name.length(); i++)
+		for (auto i = 0; i < m_PatternStr.length(); i++)
 		{
-			if (m_Name[i] == ' ')
+			if (m_PatternStr[i] == ' ') /*skip spaces*/
 				continue;
 
-			if (m_Name[i] == '?')
+			if (m_PatternStr[i] == '?')
 			{
-				if (i < m_Name.length() - 1)
-					if (m_Name[i + 1] == '?')
-						i++;
-				m_PatternBytes.push_back(00); /*it's a wildcard, we don't care of the value*/
+				if (m_PatternStr[i + 1] == '?')
+					i++;
+
+				m_PatternBytes.push_back(0); /*we don't take care of wildcards*/
+
+				continue;
 			}
 
-			char Characters[2] = { m_Name[i], m_Name[i + 1]};
-			auto Converted = strtoul(Characters, nullptr, 16); /*convert the string HEX number(base of 16) to an unsigned
-																long number*/
+			char Bytes[] = { m_PatternStr[i], m_PatternStr[i + 1] }; /*store the 2bytes in a single char*/
 
-			m_PatternBytes.push_back(std::uint8_t(Converted));
+			auto ConvertedBytes = strtoull(Bytes, nullptr, 16);
+
+			m_PatternBytes.push_back(std::uint8_t(ConvertedBytes));
+
+			i++;
 		}
 	}
 
@@ -48,13 +52,14 @@ namespace change_me
 	{}
 	bool PatternScanner::Initialize()
 	{
-		auto ModuleMgr = g_ComponentMgr->GetComponent<ModuleManager>("ModuleMgr");
-		ModuleMgr->AddModule(std::make_shared<Module>("GoW.exe")); /*add the game module itself*/
+		auto ModuleMgr = g_ComponentMgr->GetComponent<ModuleManager>("ModuleManager");
+		ModuleMgr->AddModule(std::make_shared<Module>(g_GameModuleName)); /*add the game module itself*/
 
 		return true;
 	}
 	bool PatternScanner::Run()
 	{
+		Scan();
 		return true;
 	}
 	bool PatternScanner::Uninitialize()
@@ -77,34 +82,51 @@ namespace change_me
 	}
 	void PatternScanner::Scan()
 	{
+		LOG(INFO) << "Scanning patterns...";
 		for (auto& Pat : m_Patterns)
 		{
-			auto PatternBytes = Pat.first->GetPatternBytes();
-			for (auto i = Pat.second->GetBase(); i < Pat.second->GetEnd(); i++)
+			for (std::uintptr_t i = Pat.second->GetBase(); i < Pat.second->GetEnd(); i++) /*walk throught each game addr*/
 			{
-				auto CurOpCodes = reinterpret_cast<std::uint8_t*>(i);
-				for (auto CurOpCodeId = 0; i < PatternBytes.size(); CurOpCodeId++)
+				auto OpCodes = reinterpret_cast<std::uint8_t*>(i);
+				auto PatternBytes = Pat.first->GetPatternBytes();
+
+				for (auto CurOpId = 0; CurOpId < PatternBytes.size(); CurOpId++)
 				{
-					if (PatternBytes[CurOpCodeId] == 0) /*its a wildcard so we don't care of the byte*/
-						continue;
+					auto CurOpCode = OpCodes[CurOpId];
 
-					if (PatternBytes[CurOpCodeId] != CurOpCodes[CurOpCodeId])
-						break; 
+					if (PatternBytes[CurOpId] == 0)
+						continue; /*the current op code is a wildcard in our signature, so we don't care about it*/
 
-					/*if we reached this place the pattern have been found*/
-					auto Ptr = PointerMath(i);
+					else if (PatternBytes[CurOpId] != OpCodes[CurOpId])
+						break;
 
-					if (Pat.first->OnAdjustPtr)
-						Pat.first->OnAdjustPtr(Ptr);
+					if (CurOpId == PatternBytes.size() - 1) /*if the loop reached this place, we found out the pattern*/
+					{
 
-					if (Pat.first->OnGetPtrsFromPtr)
-						Pat.first->OnGetPtrsFromPtr(Ptr);
+						auto Ptr = PointerMath(i);
 
-					if (Pat.first->OnFinalPtr)
-						Pat.first->OnFinalPtr(Ptr);
+						LOG(INFO) << "{ " 
+							<< AddColorToStream(LogColor::CYAN) << "PatternName: " << Pat.first->GetName() << ","
+							<< "\t" << AddColorToStream(LogColor::MAGENTA) << "FoundAddress: " << std::hex << i 
+							<<  ResetStreamColor << " }";
 
-					i = Pat.second->GetEnd(); /*to ensure we don't continue scanning*/
+						if (Pat.first->OnAdjustPtr)
+							Pat.first->OnAdjustPtr(Ptr);
+
+						if (Pat.first->OnGetPtrsFromPtr)
+							Pat.first->OnGetPtrsFromPtr(Ptr);
+
+						if (Pat.first->OnFinalPtr)
+							Pat.first->OnFinalPtr(Ptr);
+
+						i = Pat.second->GetEnd() + 1; /*to ensure we don't continue scanning*/
+
+						break;
+					}
 				}
+
+				if (i == Pat.second->GetEnd())
+					LOG(WARNING) << "Error, could not retrieve pattern " << Pat.first->GetName();
 			}
 		}
 	}
