2e7cac9642ee5ff3a8ff9b184ccb089ad8c1c1e8
danitrchetadas <danitrchetadas@gmail.com>
2022-03-04 20:56:23 +0100

    feat(Common): Included g3log headers and added a method to create a dummy swap chain to be able to render.

--- src/Common.hpp
+++ src/Common.hpp
@@ -20,13 +20,29 @@
 #include <functional>
 #include <mutex>
 
+#pragma comment (lib, "d3d11.lib")
+#include <d3d11.h>
+
+#include <MinHook.h>
+
+#include <imgui.h>
+#include <backends/imgui_impl_win32.h>
+#include <backends/imgui_impl_dx11.h>
+
+#include <g3log/g3log.hpp>
+#include <g3log/loglevels.hpp>
+#include <g3log/logworker.hpp>
+
 #include "Memory/PointerMath.hpp"
 
-#include "Logger.hpp" /*logger should not be modified very often*/
+#include "Pools/Thread/ThreadPool.hpp" /*pools shouldn't be modified*/
+#include "Pools/Fiber/FiberPool.hpp"
 
 #include "FileManager/File.hpp"
 #include "FileManager/Folder.hpp"
-#include "FileManager.hpp"
+#include "FileManager/FileManager.hpp"
+
+#include "Logger.hpp" /*logger should not be modified very often*/
 
 #include "ComponentManager/ComponentBase.hpp" /*the component system should not even be modified*/
 #include "ComponentManager/ComponentManager.hpp"
@@ -34,6 +50,8 @@
 #include "Components/ModuleManager/ModuleManager.hpp" /*same with module manager*/
 
 #include "Components/PatternScanner/PatternScanner.hpp" /*same with the pattern scanner*/
+
+#include "Components/Hooking/Hook.hpp"
 
 namespace change_me
 {
@@ -46,4 +64,6 @@ namespace change_me
 	extern DWORD		g_MainThreadId;
 
 	extern std::string_view g_GameModuleName; /*for ex: GoW.exe || GTA5.exe */
+
+	extern std::uintptr_t** GetSwapChainTable();
 }
