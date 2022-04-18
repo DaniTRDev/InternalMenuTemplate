3618a350235fb177385e598e7987daf6a552b817
danitrchetadas <danitrchetadas@gmail.com>
2022-04-18 11:39:03 +0200

    feat(Pointers): Made pointers a singleton and added a few macros to easily cast pointers.

--- src/Threads/Pointers/Pointers.cpp
+++ src/Threads/Pointers/Pointers.cpp
@@ -4,88 +4,60 @@
 
 namespace change_me
 {
-	std::shared_ptr<Pointers> g_Pointers;
-	Pointers::Pointers() :
-		ThreadPoolBase(
-			[](void* Param)
-			{
-				auto Ptrs = reinterpret_cast<Pointers*>(Param);
-				Ptrs->Initialize();
-
-				g_ThreadPool->OnThreadEvent(Ptrs->m_ThreadHandle, ThreadEvent::ThreadEvent_Initialized);
-
-				while (Ptrs->m_Initialized)
-				{
-					Ptrs->Run();
-					Sleep(1);
-				}
-
-			}, this, "Pointers")
+	Pointers::Pointers()
 		{}
 
 	bool Pointers::Initialize()
 	{
-		while (!g_PatternScanner->IsInitialized())
-			Sleep(1);
-
 		m_Hwnd = FindWindowA("SonySantaMonica", "God of War");
 
-		g_PatternScanner->OnModule(g_ModuleManager->GetModule(g_GameModuleName), [&] 
+		PatternScanner::Get()->OnModule(ModuleManager::Get()->GetModule(Joaat(g_GameModuleName)), []
 		{
-				g_PatternScanner->Add({ "40 55 53 56 57 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 10", "Test1" }, 
-					[this](PointerMath& Ptr)
+				PatternScanner::Get()->Add({ "40 55 53 56 57 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 10", "Test1" },
+					[](PointerMath& Ptr)
 					{
-						m_Test1 = decltype(m_Test1)("Test1", Ptr.As<std::uintptr_t>(), (void*)WndProcHK);
+						DHOOK_DECL(Pointers::Get()->m_Test1, "Test1", WndProcHK, Ptr, As<std::uintptr_t>());
 					});
 
-				g_PatternScanner->Add({ "48 8B 0D ? ? ? ? 8B D8 85 DB", "Device" },
-					[this](PointerMath& Ptr)
+				PatternScanner::Get()->Add({ "48 8B 0D ? ? ? ? 8B D8 85 DB", "Device" },
+					[](PointerMath& Ptr)
 					{
-						m_Device = decltype(m_Device)("Device", *Ptr.ReadRel(3).As<std::uintptr_t*>());
+						POINTER_DECL(Pointers::Get()->m_Device, "Device", Ptr, ReadRel(3).DerreferenceAsAddr());
 
-						ID3D11DeviceContext* Ctx;
-						(*Ptr.ReadRel(3).As<ID3D11Device**>())->GetImmediateContext(&Ctx);
+						ID3D11DeviceContext* Ctx = nullptr;
+						Pointers::Get()->m_Device.GetPtr()->GetImmediateContext(&Ctx);
 
-						m_Context = decltype(m_Context)("Context", std::uintptr_t(Ctx));
-
-					});
-
-				g_PatternScanner->Add({ "2B 45 FB 89 05", "Height" },
-					[this](PointerMath& Ptr)
-					{
-						m_Height = decltype(m_Height)("Height", Ptr.Add(3).ReadRel(2));
+						POINTER_DECL_RAW(Pointers::Get()->m_Context, "Context", Ctx);
 
 					});
 
-				g_PatternScanner->Add({ "89 05 ? ? ? ? 8B 45 03", "Width" },
-					[this](PointerMath& Ptr)
+				PatternScanner::Get()->Add({ "2B 45 FB 89 05", "Height" },
+					[](PointerMath& Ptr)
 					{
-						m_Width = decltype(m_Width)("Width", Ptr.ReadRel(2));
-
+						POINTER_DECL(Pointers::Get()->m_Height, "Height", Ptr, Add(3).ReadRel(2));
 					});
-		});
 
-		g_PatternScanner->DoScan();
-		m_Initialized = true;
+				PatternScanner::Get()->Add({ "89 05 ? ? ? ? 8B 45 03", "Width" },
+					[](PointerMath& Ptr)
+					{
+						POINTER_DECL(Pointers::Get()->m_Width, "Width", Ptr, ReadRel(2));
+					});
+
+				PatternScanner::Get()->Add({ "89 05 ? ? ? ? 8B 45 03 8B 8B", "FAIL_TEST" },
+					[](PointerMath& Ptr)
+					{
+						
+					});
+			}, []() /*a callback that will be invoked when every pattern has been scanned*/
+			{
+				auto SwapTable = GetSwapChainTable();
+
+				DHOOK_DECL_RAW(Pointers::Get()->m_SwapChain, "SwapChain", PresentHK, SwapTable[g_SwapChainPresentId]);
+
+				Hooking::Get()->DoHooks();
+
+			});
 
 		return true;
-	}
-	bool Pointers::Run()
-	{
-		if (g_PatternScanner->AreCriticalPatternsScanned())
-		{
-			static std::once_flag Flag;
-			std::call_once(Flag, [&]
-				{
-					auto SwapTable = GetSwapChainTable();
-
-					m_SwapChain = decltype(m_SwapChain)("SwapChain", std::uintptr_t(SwapTable[g_SwapChainPresentId]),
-						(void*)PresentHK);
-				});
-		}
-		return true;
-	}
-	void Pointers::UnitializeThread()
-	{
 	}
 }
