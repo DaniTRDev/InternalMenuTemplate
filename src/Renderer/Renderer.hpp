8eebfb3d17acae6839f71d1fba6b21d5f8ccabdf
danitrchetadas <danitrchetadas@gmail.com>
2022-04-18 11:35:07 +0200

    feat(Renderer): Added a function to specifically initialize ImGui and a few more to mess with D3D11.

--- src/Renderer/Renderer.hpp
+++ src/Renderer/Renderer.hpp
@@ -2,13 +2,14 @@
 
 namespace change_me
 {
-	class Renderer
+
+	class Renderer : public Singleton<Renderer>
 	{
 	public:
 
 		Renderer();
 		
-		void Initialize();
+		void Initialize(ID3D11Device* Device, ID3D11DeviceContext* DeviceContext, IDXGISwapChain* SwapChain);
 		void Uninitialize();
 
 	public:
@@ -21,35 +22,46 @@ namespace change_me
 
 	public:
 
-		bool SetD3DPointers(ID3D11Device * Device, ID3D11DeviceContext* DeviceContext, IDXGISwapChain * SwapChain);
-
 		bool CreateRenderTarget();
 		bool CreateBlendState();
+
+		ID3D11Texture2D* Create2DTexture(D3D11_TEXTURE2D_DESC * Desc, D3D11_SUBRESOURCE_DATA* ResourceData);
+		ID3D11ShaderResourceView* CreateShaderResourceView(ID3D11Texture2D* Texture, D3D11_SHADER_RESOURCE_VIEW_DESC* Desc);
+
+		ID3D11ShaderResourceView* CreateShaderResourceViewForImg(void* Pixels, std::uint32_t Width, std::uint32_t Height, std::string ImgName);
 
 		void PreResize();
 		void PostResize();
 
+		ID3D11Device* GetDevice();
+		ID3D11DeviceContext* GetContext();
+
+		IDXGISwapChain* GetSwapChain();
+
 	private:
 
-		Timer m_OpenTimer;
+		void InitializeImGui();
+
+	private:
+
 		bool m_Open;
-
-		std::unique_ptr<NotificationManager> m_Notifications;
-
 		bool m_Initialized;
 
-	private:
+		std::unique_ptr<NotificationManager> m_Notifications;
+		std::unique_ptr<UIManager> m_UIManager;
+
+	private: /*d3d11 stuff*/
 
 		ID3D11Device* m_Device; /*we save a local copy to make our live easier while coding*/
 		ID3D11DeviceContext* m_DeviceContext;
 		IDXGISwapChain* m_SwapChain;
 
-		ID3D11RenderTargetView* m_RenderTarget; /*these things are to fix some problems in games
+		ID3D11RenderTargetView* m_RenderTarget; /*these things will fix some problems in games
 												 of imgui not rendering or the alpha getting broken*/
 		ID3D11BlendState* m_BlendState;
 		float m_BlendColor[4];
 
-	}; extern std::shared_ptr<Renderer> g_Renderer;
+	};
 }
 
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND Hwnd, UINT Msg, WPARAM wParam, LPARAM lParam);
