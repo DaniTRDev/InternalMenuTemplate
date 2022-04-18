6c1066ff1d145f88821b0457bf36364cc014c95d
danitrchetadas <danitrchetadas@gmail.com>
2022-04-18 11:32:50 +0200

    feat(Main.hpp): Added a basic tab.

--- src/Menu/Main.hpp
+++ src/Menu/Main.hpp
@@ -3,7 +3,19 @@
 namespace change_me
 {
 
-	inline void MainMenu()
+	inline void MainMenu(UIManager * UI)
 	{
+		if (UI->AddTab("MainTab", 0, 0))
+		{
+			if (ImGui::Begin("asd1"))
+			{
+				ImGui::Button("asd1");
+				if (ImGui::Button("test"))
+				{ }
+				
+
+			} ImGui::End();
+		} UI->PopTab();
+		
 	}
 }
